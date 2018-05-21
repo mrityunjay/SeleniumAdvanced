9730344a5921a41e4649f73cc01e79ac468659e6
mrityunjay <mrityunjay@DESKTOP-950E3SU>
2018-05-21 12:35:47 +0530

    added wait method

--- Selenium_Advanced/src/com/test/SeleniumTest.java
+++ Selenium_Advanced/src/com/test/SeleniumTest.java
@@ -31,6 +31,15 @@ public class SeleniumTest {
 		driver.findElement(By.name("login")).click();
 	}
 	
+	public static void waitForLoad(int sec) {
+		try {
+			Thread.sleep(sec*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static void registerUser(WebDriver driver) {
 
 		//Click on Register Link
